a85da37fee984da1937982ec44cf628ca017c1ef
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-07-31 08:39:05 -0300

    Add files via upload

---
+++ bibliotecario.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Bibliotecário</title>
+  </head>
+  <body>
+    <h2>Consultas</h2>
+       <h4>Consultar usuário</h4>
+
+       <h4>Consultar livro</h4>
+          <label for="titulo">Título:</label>
+          <input type="text" name="titulo" value="">
+
+          <label for="ISBN">ISBN:</label>
+          <input type="text" name="ISBN" value="">
+
+          <label for="autor">Autor:</label>
+          <input type="text" name="autor" value="">
+
+          <button type="submit" name="button">Consultar</button>
+       <h4>Reservas de livros</h4>
+
+    <h2>Alterações</h2>
+       <h4>Empréstimo de livro</h4>
+          <label for="titulo">Título:</label>
+          <input type="text" name="titulo" value="">
+
+          <label for="ISBN">ISBN:</label>
+          <input type="text" name="ISBN" value="">
+
+          <button type="submit" name="button">Emprestar</button>
+       <h4>Devolução</h4>
+          <label for="titulo">Título:</label>
+          <input type="text" name="titulo" value="">
+
+          <label for="ISBN">ISBN:</label>
+          <input type="text" name="ISBN" value="">
+
+          <button type="submit" name="button">Devolver</button>
+
+  </body>
+</html>
