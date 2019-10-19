0fd0df0d02f1483fdbce9957a600304d3408bb49
VarnerDamascenoJr <varnerdjr@gmail.com>
2019-10-19 16:35:51 -0500

    Importação do projeto inicial

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const handlebars = require('express-handlebars')
+const app     = express()
+const bodyParser = require('body-parser')
+const path       = require('path') 
+const admin = require('./routes/admin')
+const mysql = require('mysql')
+//SETTING
+    //SESSION
+    //MIDDLEWARE
+    //BODY-PARSER
+
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
+    //HANDLEBARS
+
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+
+    //DATABASE -> MYSQL
+    var con = mysql.createConnection({
+        host:'localhost',
+        user:'varner',
+        password:'varner',
+        database:'biblioteca'
+    })
+
+    
+   /*     con.connect((err)=>{
+            if(err) throw err;
+            console.log("Connected to DataBase.")})
+          //    var sql = "SELECT*FROM pessoa"
+         // con.query(sql, (err, result)=>{
+        // if(err) throw err;
+       // console.log("1 dado inserido")
+      // })
+     // })*/
+    
+     app.get('/alunos', (req, res)=>{
+        con.query('SELECT*FROM aluno', (err, rows, fields)=>{
+            if(!err)
+            res.send(rows)
+            else
+            console.log(err)
+        })
+    })
+    
+
+
+    //PUBLIC
+    app.use(express.static(path.join(__dirname, "public")))
+    //ROUTES
+    app.use('/admin', admin)
+
+   
+
+
+const PORT = 8081
+app.listen(PORT, (req, res)=>{
+    console.log("Server connected.")
+})
---
+++ routes/admin.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const mysql   = require('mysql')
+const rota   = express.Router()
+
+var con = mysql.createConnection({
+    host:'localhost',
+    user:'varner',
+    password:'varner',
+    database:'biblioteca'
+})
+
+rota.get('/', (req, res)=>{
+    res.render('./admin/aluno/aluno')
+})
+
+
+rota.get('/aluno', (req, res)=>{
+   con.query('SELECT*FROM aluno', (err, rows, fields)=>{
+        if(!err)
+        res.send(rows)
+        else
+        console.log(err)
+    })        
+})
+
+rota.get('/aluno/add', (req, res)=>{
+    res.render('./admin/aluno/alunoadd')
+})
+
+// rotas para professor
+rota.get('/professor', (req, res)=>{
+    res.render('./admin/professor/professor')
+
+})
+
+rota.get('/professor/add', (req, res)=>{
+    res.render('./admin/professor/professoradd')
+})
+
+
+
+
+
+module.exports = rota
